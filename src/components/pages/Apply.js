d05b5806b5a0d9adcd1d3c41dba2b0bbd861d517
Kelvin Nyambe <knyambedev@gmail.com>
2022-07-28 16:46:57 +0200

    added forms to the application page

--- src/components/pages/Apply.js
+++ src/components/pages/Apply.js
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect} from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import CommunityProject from "../forms/CommunityProject";
+import EmpowermentGrant from "../forms/EmpowermentGrant";
+import SkillsTrainingBursary from "../forms/SkillsTrainingBursary";
+import EmpowermentLoan from "../forms/EmpowermentLoan";
+import SecondaryBoardingBursary from "../forms/SecondaryBoardingBursary";
 
 const Apply = () => {
-  return (
-    <main>Apply</main>
-  )
-}
+  const [applicationForm, setApplicationForm] = useState(
+    "selectApplicationForm"
+  );
 
-export default Apply
+  const [communityProjectFormVisible, setCommunityProjectFormVisible] =
+    useState(false);
+  const [emporwerentGrantFormVisible, setEmporwerentGrantFormVisible] =
+    useState(false);
+  const [empowermentLoanFormVisible, setEmpowermentLoanFormVisible] =
+    useState(false);
+  const [
+    SecondaryBoardingBursaryFormVisible,
+    setSecondaryBoardingBursaryFormVisible,
+  ] = useState(false);
+  const [
+    skillsTrainingBursaryFormVisible,
+    setSkillsTrainingBursaryFormVisible,
+  ] = useState(false);
+
+  useEffect(() => {
+    applicationForm === "communityProject"
+      ? setCommunityProjectFormVisible(true)
+      : setCommunityProjectFormVisible(false);
+    applicationForm === "empowermentGrant"
+      ? setEmporwerentGrantFormVisible(true)
+      : setEmporwerentGrantFormVisible(false);
+    applicationForm === "empowermentLoan"
+      ? setEmpowermentLoanFormVisible(true)
+      : setEmpowermentLoanFormVisible(false);
+    applicationForm === "secondaryBoardingBursary"
+      ? setSecondaryBoardingBursaryFormVisible(true)
+      : setSecondaryBoardingBursaryFormVisible(false);
+    applicationForm === "skillsDevelopmentBursary"
+      ? setSkillsTrainingBursaryFormVisible(true)
+      : setSkillsTrainingBursaryFormVisible(false);
+  }, [applicationForm]);
+
+  const handleFormChange = (e) => {
+    setApplicationForm(e.target.value);
+  };
+
+  const renderSelectionResult = () => {
+    let result;
+    applicationForm === "selectApplicationForm"
+      ? (result = "Select Application Form")
+      : (result = applicationForm);
+    return result;
+  };
+
+  return (
+    <main>
+      <h2>Types of Projects For Funding</h2>
+      <p>These are the main categories of funding under CDF</p>
+      <ul>
+        <li>Community Projects</li>
+        <li>Emporwerent Grant</li>
+        <li>Empowerment Loan</li>
+        <li>Secondary Boarding School Bursaries</li>
+        <li>Skills Development Bursaries</li>
+      </ul>
+      <p>
+        Detailed information about each category can be found in the guide
+        section.
+      </p>
+      <p>You are applying for {renderSelectionResult()}</p>
+
+      <FormControl fullWidth>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={applicationForm}
+          onChange={handleFormChange}
+        >
+          <MenuItem value="selectApplicationForm">
+            Select Application Form
+          </MenuItem>
+          <MenuItem value="communityProject">
+            Community Project Application Form
+          </MenuItem>
+          <MenuItem value="empowermentGrant">
+            Emporwerent Grant Application Form
+          </MenuItem>
+          <MenuItem value="empowermentLoan">
+            Empowerment Loan Application Form
+          </MenuItem>
+          <MenuItem value="secondaryBoardingBursary">
+            Secondary Boarding School Bursaries
+          </MenuItem>
+          <MenuItem value="skillsDevelopmentBursary">
+            Skills Development Bursaries
+          </MenuItem>
+        </Select>
+      </FormControl>
+
+      {communityProjectFormVisible && <CommunityProject />}
+      {emporwerentGrantFormVisible && <EmpowermentGrant />}
+      {empowermentLoanFormVisible && <EmpowermentLoan />}
+      {SecondaryBoardingBursaryFormVisible && <SecondaryBoardingBursary />}
+      {skillsTrainingBursaryFormVisible && <SkillsTrainingBursary />}
+    </main>
+  );
+};
+
+export default Apply;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,12 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
-import CameraIcon from "@mui/icons-material/PhotoCamera";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const Home = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -57,7 +45,9 @@ const Home = () => {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained" href="/apply">Apply</Button>
+              <Button variant="contained" href="/apply">
+                Apply
+              </Button>
               <Button variant="outlined">Search</Button>
             </Stack>
           </Container>
